95ec0124d6eb9cde92433db0a095ada3c87fd4c4
IbragimKhaniev <newinterny@gmail.com>
2025-11-24 00:09:42 +0300

    Add is_authenticated and is_anonymous properties to Member model

--- api/models.py
+++ api/models.py
@@ -7,6 +7,14 @@ class Member(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
     def __str__(self):
         return self.username
 
